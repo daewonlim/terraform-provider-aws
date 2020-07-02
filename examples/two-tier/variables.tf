5de60fffb35566246bd87d455c4fdd60cf3f1a97
daewonlim <67682623+daewonlim@users.noreply.github.com>
2020-07-02 17:49:16 +0900

    Update variables.tf

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -12,9 +12,12 @@ variable "key_name" {
   description = "Desired name of AWS key pair"
 }
 
+variable "private_key_path" {
+  description = "private key"
+}
+
 variable "aws_region" {
   description = "AWS region to launch servers."
-  #default     = "us-west-2"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
@@ -23,10 +26,6 @@ variable "aws_amis" {
     eu-west-1 = "ami-674cbc1e"
     us-east-1 = "ami-1d4e7a66"
     us-west-1 = "ami-969ab1f6"
-    us-west-2 = "ami-8803e0f0"
+    us-west-2 = "ami-003634241a8fcdec0"
   }
 }
-
-variable "private_key_path" (
-  #default = "./ssh/my_pr.pem"
-)
